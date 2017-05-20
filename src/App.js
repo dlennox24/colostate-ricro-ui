5952d8c31ae7cb9055d3b2ba4d79d5bc8cea94b7
Daniel Lennox <dlennox2424@gmail.com>
2017-05-19 22:47:29 -0600

    Adding CSU footer, sticky header, and "contact" button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, {
-   Component
+  Component
 } from 'react';
+import $ from 'jquery';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import {
-   Toolbar,
-   ToolbarTitle,
-   ToolbarGroup,
-   ToolbarSeparator
+  Toolbar,
+  ToolbarTitle,
+  ToolbarGroup,
+  ToolbarSeparator
 } from 'material-ui/Toolbar';
 import FontIcon from 'material-ui/FontIcon';
 
-import CsuSvgLogo, {CsuFooter} from './CsuBranding';
+import CsuSvgLogo, {
+  CsuFooter
+} from './CsuBranding';
 
 import config from './config.json';
 import './App.css';
@@ -21,34 +24,46 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 document.title = document.title === '' ? config.appName + ' - ' + config.unitTitle : document.title;
 
 class App extends Component {
-   render() {
-      return (
-         <div>
-            <AppBar
-               iconElementLeft={<CsuSvgLogo />} />
-            <Toolbar>
-               <ToolbarTitle text={config.appName} />
-               <ToolbarGroup>
-                  <IconButton
-                     href='/'
-                     tooltip='Home'>
-                     <FontIcon className='material-icons'>home</FontIcon>
-                  </IconButton>
-                  <ToolbarSeparator style={{marginLeft:0}} />
-                  <IconButton
-                     href={config.unitContact}
-                     tooltip='Contact Us'
-                     tooltipPosition='bottom-left'>
-                     <FontIcon className='material-icons'>email</FontIcon>
-                  </IconButton>
-               </ToolbarGroup>
-            </Toolbar>
-            <div id="main-content">
-               <h2 className='text-center'>#main-content</h2>
-            </div>
-            <CsuFooter/>
-         </div>
-      );
-   }
+  componentDidMount() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1) {
+        $('#logobar').addClass('display-none');
+        $('#top-toolbar').addClass('sticky');
+      } else {
+        $('#logobar').removeClass('display-none');
+        $('#top-toolbar').removeClass('sticky');
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <AppBar
+          id='logobar'
+          iconElementLeft={<CsuSvgLogo />} />
+        <Toolbar id='top-toolbar'>
+          <ToolbarTitle text={config.appName} />
+          <ToolbarGroup>
+            <IconButton
+              href='/'
+              tooltip='Home'>
+              <FontIcon className='material-icons'>home</FontIcon>
+            </IconButton>
+            <ToolbarSeparator style={{marginLeft:0}} />
+            <IconButton
+              href={config.unitContact}
+              tooltip='Contact Us'
+              tooltipPosition='bottom-left'>
+              <FontIcon className='material-icons'>email</FontIcon>
+            </IconButton>
+          </ToolbarGroup>
+        </Toolbar>
+        <div id='main-content'>
+          <h2 className='text-center'>#main-content</h2>
+        </div>
+        <CsuFooter/>
+      </div>
+    );
+  }
 }
 export default App;
