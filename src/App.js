2250ae823bb6dcb6615c304b866eabcd7f7ec235
Daniel Lennox <dlennox2424@gmail.com>
2017-05-19 20:14:52 -0600

    Adding CSU footer

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
 import React, {
-  Component
+   Component
 } from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import {
-  Toolbar,
-  ToolbarTitle,
-  ToolbarGroup
+   Toolbar,
+   ToolbarTitle,
+   ToolbarGroup,
+   ToolbarSeparator
 } from 'material-ui/Toolbar';
+import FontIcon from 'material-ui/FontIcon';
 
-import CsuSvgLogo from './CsuBranding';
+import CsuSvgLogo, {CsuFooter} from './CsuBranding';
 
 import config from './config.json';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 document.title = document.title === '' ? config.appName + ' - ' + config.unitTitle : document.title;
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <AppBar
-          iconElementLeft={<CsuSvgLogo />} />
-        <Toolbar>
-          <ToolbarTitle text={config.appName} />
-          <ToolbarGroup>
-            <IconButton
-              href='/'
-              tooltip='Home'
-              iconClassName='fa fa-home' />
-          </ToolbarGroup>
-        </Toolbar>
-        <div id="main-content">
-          <h2 className='text-center'>#main-content</h2>
-        </div>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div>
+            <AppBar
+               iconElementLeft={<CsuSvgLogo />} />
+            <Toolbar>
+               <ToolbarTitle text={config.appName} />
+               <ToolbarGroup>
+                  <IconButton
+                     href='/'
+                     tooltip='Home'>
+                     <FontIcon className='material-icons'>home</FontIcon>
+                  </IconButton>
+                  <ToolbarSeparator style={{marginLeft:0}} />
+                  <IconButton
+                     href={config.unitContact}
+                     tooltip='Contact Us'
+                     tooltipPosition='bottom-left'>
+                     <FontIcon className='material-icons'>email</FontIcon>
+                  </IconButton>
+               </ToolbarGroup>
+            </Toolbar>
+            <div id="main-content">
+               <h2 className='text-center'>#main-content</h2>
+            </div>
+            <CsuFooter/>
+         </div>
+      );
+   }
 }
 export default App;
--- src/CsuBranding.js
+++ src/CsuBranding.js
@@ -1,32 +1,65 @@
 import React, {
-  Component
+   Component
 } from 'react';
 import $ from 'jquery';
+
 import config from './config.json';
+import './CsuBranding.css';
 
 export default class CsuSvgLogo extends Component {
-  componentDidMount() {
-    $.getScript('https://static.colostate.edu/logo/reslogo/logo.min.js').done(function() {
-      $('#unit-title').removeClass('display-none');
-    });
-  }
-  render() {
-    return (
-      <div style={{marginTop: '-7px'}} className='signature'>
-        <section id='BrandLogo' className='fontLarge'>
-          <div className='responsiveLogoContainer'>
-            <div id='responsiveLogo' className='screenMD'>
+   componentDidMount() {
+      $.getScript('https://static.colostate.edu/logo/reslogo/logo.min.js').done(function() {
+         $('#unit-title').removeClass('display-none');
+      });
+   }
+   render() {
+      return (
+         <div style={{marginTop: '-7px'}} className='signature'>
+            <section id='BrandLogo' className='fontLarge'>
+               <div className='responsiveLogoContainer'>
+                  <div id='responsiveLogo' className='screenMD'>
+                  </div>
+                  <div id='responsiveLogoSubsytem'>
+                     <h2>
+                        <a id='unit-title' className='display-none' href={config.unitUrl}>
+                           {config.unitTitle}
+                        </a>
+                     </h2>
+                  </div>
+               </div>
+            </section>
+         </div>
+      );
+   }
+}
+
+export const CsuFooter = () => {
+   return (
+      <div className="footer">
+      <footer>
+         <div className="bottom-footer">
+            <div className="container">
+               <div className="row">
+                  <div className="col col-lg-8 col-md-6">
+                     <ul>
+                        <li><a href="https://www.colostate.edu/contact/">Contact CSU</a></li>
+                        <li><a href="https://www.colostate.edu/equal-opportunity">Equal Opportunity</a></li>
+                        <li><a href="https://www.colostate.edu/privacy">Privacy Statement</a></li>
+                        <li><a href="https://www.colostate.edu/disclaimer">Disclaimer</a></li>
+                     </ul>
+                     <p className="copyright">&copy; 2017 Colorado State University, Fort Collins, Colorado 80523 USA</p>
+                  </div>
+                  <div className="col col-lg-4 col-md-6">
+                     <div className="bottomlogo">
+                        <a href="http://www.colostate.edu/">
+                           <img src="https://www.colostate.edu/wp-content/themes/csuhome-theme/assets/img/csu-logo-oneline.svg" alt=""/>
+                        </a>
+                     </div>
+                  </div>
+               </div>
             </div>
-            <div id='responsiveLogoSubsytem'>
-              <h2>
-                <a id='unit-title' className='display-none' href={config.unitUrl}>
-                  {config.unitTitle}
-                </a>
-              </h2>
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
+         </div>
+      </footer>
+   </div>
+   );
 }
