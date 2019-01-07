16b8300aecd49ccaa05922ffbd6c8111f04a77ae
Daniel Lennox <dlennox2424@gmail.com>
2019-01-07 15:06:49 -0700

    Removing styles from MarkdownParser and importing from styles.js

--- src/component/MarkdownParser/index.js
+++ src/component/MarkdownParser/index.js
@@ -11,6 +11,7 @@ import classNames from 'classnames';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Code from '../Code';
+import styles from './styles';
 
 const StyledTableCell = withStyles(theme => ({
   head: {
@@ -19,44 +20,6 @@ const StyledTableCell = withStyles(theme => ({
     fontSize: theme.typography.subtitle1.fontSize,
   },
 }))(TableCell);
-
-const bottomBorderWidth = 3;
-const styles = theme => ({
-  root: {
-    '&>:nth-child(2)': {
-      margin: 0,
-    },
-  },
-  list: {
-    marginTop: theme.spacing.unit * 1.75,
-  },
-  listItem: {
-    marginTop: theme.spacing.unit / 2,
-  },
-  heading: {
-    marginTop: theme.spacing.unit * 4,
-  },
-  headingBold: {
-    fontWeight: 'bold',
-  },
-  headingDivider: {
-    margin: `${theme.spacing.unit}px 0 ${theme.spacing.unit * 2}px`,
-  },
-  headingLink: {
-    marginTop: -56 - bottomBorderWidth,
-    position: 'absolute',
-    '@media (min-width: 1px) and (orientation: landscape)': { marginTop: -48 - bottomBorderWidth },
-    [theme.breakpoints.up('sm')]: { marginTop: -64 - bottomBorderWidth },
-  },
-  tableRoot: {
-    width: '100%',
-    margin: `${theme.spacing.unit * 2}px 0`,
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 700,
-  },
-});
 
 /* eslint-disable react/prop-types */
 const renderers = {
