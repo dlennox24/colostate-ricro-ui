c39789a93c5a5f9337c31e50438cbbb9ca9a16e4
Daniel Lennox <dlennox2424@gmail.com>
2019-01-07 18:13:34 -0700

    Reducing redundancy in ProfileStructure

--- src/component/UserProfile/ProfileStructure/component.js
+++ src/component/UserProfile/ProfileStructure/component.js
@@ -17,6 +17,28 @@ import styles from './styles';
 
 const ProfileStructure = ({ classes, currentUserId = -1, user }) => {
   const userGroups = _.sortBy(user.userGroups, ['alias', 'userGroupTypeId']);
+  const grid = [
+    [
+      {
+        primary:
+          user.csuId === currentUserId ? (
+            <EditProfileDisplayName displayName={user.displayName} />
+          ) : (
+            user.displayName
+          ),
+        secondary: 'Display Name',
+      },
+      { primary: `${user.lastName}, ${user.firstName}`, secondary: 'Name' },
+    ],
+    [
+      { primary: user.eId, secondary: 'eID' },
+      {
+        primary: user.csuId.toString().replace(/(.{3})/g, '$1 '),
+        secondary: 'CSU ID',
+      },
+    ],
+    [{ primary: user.email, secondary: 'Email' }],
+  ];
   return (
     <Grid container>
       <Grid className={classNames(classes.column, classes.profileImg)} item xs={12} md={3}>
@@ -33,66 +55,37 @@ const ProfileStructure = ({ classes, currentUserId = -1, user }) => {
         )}
       </Grid>
       <Grid className={classes.column} item xs={12} md={9}>
-        <Grid container>
-          <Grid item xs={12}>
-            <Typography variant="h6">Account</Typography>
-            <Divider />
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item md={6} xs={12}>
-            {user.csuId === currentUserId ? (
-              <EditProfileDisplayName displayName={user.displayName} />
-            ) : (
-              <ListItem>
-                <ListItemText primary={user.displayName} secondary="Display Name" />
-              </ListItem>
-            )}
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <ListItem>
-              <ListItemText primary={`${user.lastName}, ${user.firstName}`} secondary="Name" />
-            </ListItem>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item md={6} xs={12}>
-            <ListItem>
-              <ListItemText primary={user.eId} secondary="eID" />
-            </ListItem>
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <ListItem>
-              <ListItemText
-                primary={user.csuId.toString().replace(/(.{3})/g, '$1 ')}
-                secondary="CSU ID"
-              />
-            </ListItem>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item xs={12}>
-            <ListItem>
-              <ListItemText primary={user.email} secondary="Email" />
-            </ListItem>
-          </Grid>
-        </Grid>
-        {_.isEmpty(userGroups) ? null : (
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography className={classes.groupsHeader} variant="h6">
-                Groups
-              </Typography>
-              <Divider />
-              <List disablePadding>
-                {userGroups.map(userGroup => (
-                  <ListItem key={userGroup.alias}>
-                    <ListItemText primary={userGroup.alias} secondary={userGroup.description} />
+        <Typography variant="h5">Account</Typography>
+        <Divider />
+        {grid.map(row => (
+          <Grid key={row[0].secondary} container>
+            {row.map(cell => (
+              <Grid key={cell.secondary} item xs={12} md={12 / row.length}>
+                {React.isValidElement(cell.primary) ? (
+                  cell.primary
+                ) : (
+                  <ListItem component="div">
+                    <ListItemText primary={cell.primary} secondary={cell.secondary} />
                   </ListItem>
-                ))}
-              </List>
-            </Grid>
+                )}
+              </Grid>
+            ))}
           </Grid>
+        ))}
+        {_.isEmpty(userGroups) ? null : (
+          <React.Fragment>
+            <Typography className={classes.groupsHeader} variant="h5">
+              Groups
+            </Typography>
+            <Divider />
+            <List disablePadding>
+              {userGroups.map(userGroup => (
+                <ListItem key={userGroup.alias}>
+                  <ListItemText primary={userGroup.alias} secondary={userGroup.description} />
+                </ListItem>
+              ))}
+            </List>
+          </React.Fragment>
         )}
       </Grid>
     </Grid>
