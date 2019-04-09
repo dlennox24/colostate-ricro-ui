c2ca0938721ba5ebe3497929c4a2e828621755fd
Daniel Lennox <dlennox2424@gmail.com>
2019-04-09 13:41:32 -0600

    [3.4.2] Removing dead code from heading styles in MarkdownParser

--- src/component/MarkdownParser/styles.js
+++ src/component/MarkdownParser/styles.js
@@ -1,33 +1,10 @@
 /* eslint-disable max-lines-per-function */
-const bottomBorderWidth = 3;
 const styles = theme => ({
   root: {
     '&>:nth-child(2)': { margin: 0 },
   },
   list: { margin: `0 0 ${theme.spacing.unit * 2}px` },
   listItem: { marginTop: theme.spacing.unit / 2 },
-  headingContainer: {},
-  headingLinkIcon: {
-    fontSize: 'inherit',
-    margin: `0 ${theme.spacing.unit}px`,
-    verticalAlign: 'middle',
-  },
-  heading: {
-    cursor: 'pointer',
-    marginTop: theme.spacing.unit * 4,
-    '&:hover': {
-      background: theme.palette.grey[300],
-      borderRadius: theme.spacing.unit / 2,
-    },
-  },
-  headingBold: { fontWeight: 'bold' },
-  headingDivider: { margin: `${theme.spacing.unit}px 0 ${theme.spacing.unit * 2}px` },
-  headingLink: {
-    marginTop: -56 - bottomBorderWidth,
-    position: 'absolute',
-    '@media (min-width: 1px) and (orientation: landscape)': { marginTop: -48 - bottomBorderWidth },
-    [theme.breakpoints.up('sm')]: { marginTop: -64 - bottomBorderWidth },
-  },
   image: {
     maxWidth: '100%',
     margin: `${theme.spacing.unit}px 0`,
