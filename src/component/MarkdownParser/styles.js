c6a2407ea676fb2ae01215cc606f01f9118c424b
Daniel Lennox <dlennox2424@gmail.com>
2019-04-09 13:34:39 -0600

    [3.4.2] Fixing MarkdownParser styling
    Fixes #32

--- src/component/MarkdownParser/styles.js
+++ src/component/MarkdownParser/styles.js
@@ -1,39 +1,43 @@
+/* eslint-disable max-lines-per-function */
 const bottomBorderWidth = 3;
 const styles = theme => ({
   root: {
-    '&>:nth-child(2)': {
-      margin: 0,
-    },
+    '&>:nth-child(2)': { margin: 0 },
   },
-  list: {
-    marginTop: theme.spacing.unit * 1.75,
-  },
-  listItem: {
-    marginTop: theme.spacing.unit / 2,
+  list: { margin: `0 0 ${theme.spacing.unit * 2}px` },
+  listItem: { marginTop: theme.spacing.unit / 2 },
+  headingContainer: {},
+  headingLinkIcon: {
+    fontSize: 'inherit',
+    margin: `0 ${theme.spacing.unit}px`,
+    verticalAlign: 'middle',
   },
   heading: {
+    cursor: 'pointer',
     marginTop: theme.spacing.unit * 4,
+    '&:hover': {
+      background: theme.palette.grey[300],
+      borderRadius: theme.spacing.unit / 2,
+    },
   },
-  headingBold: {
-    fontWeight: 'bold',
-  },
-  headingDivider: {
-    margin: `${theme.spacing.unit}px 0 ${theme.spacing.unit * 2}px`,
-  },
+  headingBold: { fontWeight: 'bold' },
+  headingDivider: { margin: `${theme.spacing.unit}px 0 ${theme.spacing.unit * 2}px` },
   headingLink: {
     marginTop: -56 - bottomBorderWidth,
     position: 'absolute',
     '@media (min-width: 1px) and (orientation: landscape)': { marginTop: -48 - bottomBorderWidth },
     [theme.breakpoints.up('sm')]: { marginTop: -64 - bottomBorderWidth },
   },
+  image: {
+    maxWidth: '100%',
+    margin: `${theme.spacing.unit}px 0`,
+  },
   tableRoot: {
     width: '100%',
     margin: `${theme.spacing.unit * 2}px 0`,
     overflowX: 'auto',
   },
-  table: {
-    minWidth: 700,
-  },
+  table: { minWidth: 700 },
 });
 
 export default styles;
